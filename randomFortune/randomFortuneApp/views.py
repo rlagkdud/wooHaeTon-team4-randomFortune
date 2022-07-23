bd265b09483d8090787e2bbc9214f8ba05894d7d
rlagkdud <ha0936861@gmail.com>
2022-07-23 17:33:31 +0900

    basic template

--- randomFortune/randomFortuneApp/views.py
+++ randomFortune/randomFortuneApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(req):
+    pass
+    return render(req,'home.html')
+
+def result(req):
+    pass
+    return render(req,"result.html")
