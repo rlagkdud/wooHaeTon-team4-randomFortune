0df8d3df0b1574ec03070d077edc4fbdccb406c0
rlagkdud <ha0936861@gmail.com>
2022-07-24 21:48:55 +0900

    add getItem function

--- randomFortune/randomFortuneApp/views.py
+++ randomFortune/randomFortuneApp/views.py
@@ -1,14 +1,21 @@
+from random import random
 from django.shortcuts import render
+import random
 
 # Create your views here.
+def getItem():
+    itemList = ["모자", "립밤", "팔찌", "손거울", "손수건", "에어팟", "핸드폰", "집게핀", "시계", "셔츠", "가방", "백팩"]
+    luckyItem = random.choice(itemList)
+    return luckyItem
+
+
 def home(req):
     if(req.method == "POST"):
-        print(req.POST.get('username'))
         username = req.POST.get('username')
-        return render(req,'result.html',{'username':username})
+        luckyItem = getItem()  
+        return render(req,'result.html',{'username':username,'luckyItem':luckyItem})
     elif(req.method == "GET"):
         return render(req,'home.html')
-    
 
-def result(req):
+def result(req):  
     return render(req,"result.html")
